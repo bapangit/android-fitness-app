9c81d997296ad414b1a3b2691a1e9fa5e20abfd6
Hexaphor5 <bm971321@gmail.com>
2022-05-17 06:10:46 -0700

    created resistration fragment

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,18 +1,52 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView button_account;
+    FragmentResistration membersResistration;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        //variable initialization
+        button_account = findViewById(R.id.id_account);
+
+
+        fragmentManager = getSupportFragmentManager();
+        //fragments
+        membersResistration = new FragmentResistration();
+
+
+        //main
+        //attach listener
+        button_account.setOnClickListener(this::onClick);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.id_account){
+           FragmentTransaction ft = fragmentManager.beginTransaction();
+            ft.addToBackStack("TAG_OF_FRAGMENT");
+            ft.add(R.id.fragmentContainerView, membersResistration).commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (fragmentManager.getBackStackEntryCount() > 0)
+            fragmentManager.popBackStackImmediate();
+        else super.onBackPressed();
     }
 
 }
