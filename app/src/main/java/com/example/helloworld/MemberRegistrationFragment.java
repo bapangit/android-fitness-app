f7e88a3fb1c635ee84fc9231835a71c01d8bd340
bapangit <cmandal97531@gmail.com>
2022-05-24 22:55:22 +0530

    added member registration feature

--- app/src/main/java/com/example/helloworld/MemberRegistrationFragment.java
+++ app/src/main/java/com/example/helloworld/MemberRegistrationFragment.java
@@ -21,6 +21,6 @@ public class MemberRegistrationFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+            
     }
 }
